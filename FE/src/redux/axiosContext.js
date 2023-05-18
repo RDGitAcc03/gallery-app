afdb65fb923218b46dbfcfb4db2eee96742d4e83
RD <randahn03@gmail.com>
2023-05-18 16:51:58 +0300

    deployment urls fixing

--- FE/src/redux/axiosContext.js
+++ FE/src/redux/axiosContext.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 function getServerUrl() {
-    if (process.env.NODE_ENV === 'production') return 'https://galleryapp-67bo.onrender.com'
+    if (process.env.NODE_ENV === 'production') return 'https://galleryappbe.onrender.com'
     return "http://localhost:8080";
   }
   
